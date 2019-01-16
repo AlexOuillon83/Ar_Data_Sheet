53c1ae49dff41b99904fa29f3301f3819c7311aa
Alex OUILLON <alex.ouillon@outlook.fr>
2019-01-16 14:33:04 +0100

    First Commit
    Initializing the repo

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public void changeScene(string name)
+    {
+#pragma warning disable CS0618 // Le type ou le membre est obsolète
+        Application.LoadLevel(name);
+#pragma warning restore CS0618 // Le type ou le membre est obsolète
+        Debug.Log("Hello WORLD  ", gameObject);
+    }
+
+    public void openUrl()
+    {
+        Application.OpenURL("https://www.futbin.com/");
+    }
+}
